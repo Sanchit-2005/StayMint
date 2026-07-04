0f2754a1718ea37ff19a03a4b90a4f774ad74ba9
Sanchit Wakpanjar <vijaywakpanjar877@gmail.com>
2026-07-04 12:54:00 +0530

    added the  new route that will create a new listing

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const port = 3000;
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 
-
 const path = require("path");
 
-// Middleware to parse JSON requests
+
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -24,20 +23,39 @@ main()
     console.error("Error connecting to MongoDB:", err);
   });
 
-app.get("/listing",async(req,res)=>{
-  let listingInfo=await Listing.find({});
+//*listing all hotels
+app.get("/listing", async (req, res) => {
+  let listingInfo = await Listing.find({});
   // console.log(listingInfo);
-   res.render("listings/index",{listingInfo});
+  res.render("listings/index", { listingInfo });
+});
+
+
+//* adding new hotel to listing 
+app.get("/listing/new",(req,res)=>{
+  res.render("listings/new");
 })
 
-
-app.get("/listing/:id",async(req,res)=>{
-  let {id}=req.params;
+app.post("/listings",async(req,res)=>{
+  let listing=req.body.listing;
+  console.log(listing);
+  const list=new Listing(listing);
+  console.log(list);
+  await list.save();
+})
+//*  detailed info of each hotel
+app.get("/listing/:id", async (req, res) => {
+  let { id } = req.params;
   console.log(id);
-    const listedgData=await Listing.findById(id);
-    console.log(listedgData);
-    res.render("listings/show",{listedgData});
-})
+  const listedgData = await Listing.findById(id);
+  console.log(listedgData);
+  res.render("listings/show", { listedgData });
+});
+
+
+
+
+
 app.listen(port, (req, res) => {
   console.log("listing on port 3000");
 });
