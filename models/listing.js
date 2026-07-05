0c579821a8332b1743b82265b31840b761006226
Sanchit Wakpanjar <vijaywakpanjar877@gmail.com>
2026-07-06 01:34:45 +0530

    fixed the default image

--- models/listing.js
+++ models/listing.js
@@ -17,6 +17,7 @@ const ListingSchema = new Schema({
     },
     url: {
         type: String,
+         default: "https://images.unsplash.com/photo-1782397161807-9bab207b6d00?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
 },
   location: {
